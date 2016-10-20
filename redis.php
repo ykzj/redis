46c060a3a69225272015781eccc3d7487021cdfd
Liu, Xudong <lxd@amazon.com>
2016-10-20 10:12:32 +0800

    0.3
    0.3

--- redis.php
+++ redis.php
@@ -2,9 +2,27 @@
 	$redis = new Redis();
 	$redis->connect('demo.3d0kus.ng.0001.apne1.cache.amazonaws.com');
 	
-	$redis->set('key', 'value');
-	echo $redis->get('key');
+
+	$redis->set(0, 'value');
+	echo $redis->get(0);
 
 
 	$redis->close();
+
+	function genRandomString($len)
+	{
+		$char_begin = 33;	//!
+		$char_end = 126;	//~
+		$str = '';
+		for($i=0; $i<$len; $i++)
+		{
+			$char_int = rand($char_begin, $char_end);
+			while($char_int == 39 | $char_int == 96)	//no '
+			{
+				$char_int = rand($char_begin, $char_end);
+			}
+			$str .= chr($char_int);
+		}
+		return $str;
+	}
 ?>
