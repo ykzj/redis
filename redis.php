a584bb4fc7e860ca125466f1a4f67ebbe1cb6a26
Liu, Xudong <lxd@amazon.com>
2016-10-20 10:00:29 +0800

    0.2
    0.2

--- redis.php
+++ redis.php
@@ -1,7 +1,10 @@
 <?php
 	$redis = new Redis();
 	$redis->connect('demo.3d0kus.ng.0001.apne1.cache.amazonaws.com');
-	var_dump($redis->info());
-	$redis->close();
+	
+	$redis->set('key', 'value');
+	echo $redis->get('key');
 
+
+	$redis->close();
 ?>
